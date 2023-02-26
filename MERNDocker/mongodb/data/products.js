f58512fa6f7a57a87ef7f246422582401334b9e7
sakchaisrim <sakchaisrim@gmail.com>
2023-02-26 17:05:36 +0700

    first commit

---
+++ MERNDocker/mongodb/data/products.js
@@ -0,0 +1,19 @@
+db.createCollection("products");
+db.products.insert({
+    "id": 1,
+    "productName": "iPhone 13 Pro Max",
+    "price": "45000",
+    "thumbnail": "/image/product/1.jpg"
+});
+db.products.insert({
+    "id": 2,
+    "productName": "iPad Mini 2021",
+    "price": "25000",
+    "thumbnail": "/image/product/2.jpg"
+}); 
+db.products.insert({
+    "id": 3,
+    "productName": "apple Watch 2021",
+    "price": "15800",
+    "thumbnail": "/image/product/3.jpg"
+});  
